04555f6bd7e689d30ee349505a5062546f00b812
BezTTTT <Best_1614@hotmail.com>
2021-12-28 19:42:06 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double input[],int c,double output[]){
+    double ans0=0,ans1=0,ans2=1,ans3=0, smt=0;
+    output[4]=input[0];
+    output[5]=input[0];
+    for(int i=0;i<c;i++){
+        ans0+=input[i];
+        if(input[i]>output[4]) output[4]=input[i];
+        if(input[i]<output[5]) output[5]=input[i];
+    }output[0] = ans0/c;
+    for(int i=0;i<c;i++){
+        ans1+=pow((input[i]-output[0]),2);
+        ans2*=input[i];
+        ans3+= 1/input[i];
+    }
+    smt = (1/double(c));
+    output[1] =sqrt(ans1/c);
+    output[2] =pow(ans2,smt);
+    output[3] = c/ans3;
+}
