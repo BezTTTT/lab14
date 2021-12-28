b7202e34aff6672f92bb95f8a0d0d02d08f7a072
BezTTTT <Best_1614@hotmail.com>
2021-12-28 19:42:38 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,38 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool pic[N][M],int s,int x,int y)
+{
+        for(int i=0;i<N; i++)
+        {
+            for(int j=0; j<M; j++){
+                if (sqrt(pow(i-x,2)+pow(j-y,2))+1<=s)
+                {
+                    pic[i][j]=1;
+                }
+            }
+        }
+}
+void showImage(const bool pic[N][M]){
+    for(int count=0;count<72;count++){
+        cout <<"-";
+    }
+    cout << "\n";
+    for(int i=0;i<30;i++){
+        for(int x=0;x<71;x++){
+            if(x==0) cout << "|";
+            if(x==70) {
+                cout << "|";
+                continue;
+            }
+            if(pic[i][x]==1) cout << "*";
+            else cout << " ";
+        }
+        cout << "\n";
+    }
+    for(int count=0;count<72;count++){
+        cout <<"-";
+    }
+    cout << "\n";
+}
